840254c4cfd268f0d58506c47b4711a9f49b14a2
Felipe Munhoz <felipe@felipemunhoz.com>
2016-03-12 15:25:11 -0300

    Instantiates redux store

--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
 import style from './index.css'
+import { createStore } from 'redux'
 
 var React = require('react')
 var ReactDOM = require('react-dom')
 
 import Ticker from './ticker'
 
-ReactDOM.render(
-  <Ticker className={style.component} />,
-  document.getElementById('tomuti')
-)
+const storeManager = (state, action) => {
+  return state
+}
+
+let store = createStore(storeManager)
+
+const render = () => {
+  ReactDOM.render(
+    <Ticker value={store.getState()} className={style.component} />,
+    document.getElementById('tomuti')
+  )
+}
+
+render()
+
+store.subscribe(render)
