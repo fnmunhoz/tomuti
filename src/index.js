77f3d6bc98030fb955e7cdc2ce3efe410947e2b8
Felipe Munhoz <felipe@felipemunhoz.com>
2016-03-19 20:57:47 -0300

    Use redux-thunk as a middleware to allow async actions

--- src/index.js
+++ src/index.js
@@ -2,11 +2,18 @@ import 'babel-polyfill'
 import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
+import logger from 'redux-logger'
+import thunk from 'redux-thunk'
 import tomutiApp from './reducers'
 import App from './components/app'
 
-let store = createStore(tomutiApp)
+const middleware = process.env.NODE_ENV === 'production' ? [ thunk ] : [ thunk, logger() ]
+
+const store = createStore(
+  tomutiApp,
+  applyMiddleware(...middleware)
+)
 
 render(
   <Provider store={store}>
