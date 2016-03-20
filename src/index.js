9099963af702cacdd8fee83218ddf031ab5c0153
Felipe Munhoz <felipe@felipemunhoz.com>
2016-03-19 21:05:00 -0300

    Rename the index reducer

--- src/index.js
+++ src/index.js
@@ -5,13 +5,13 @@ import { Provider } from 'react-redux'
 import { createStore, applyMiddleware } from 'redux'
 import logger from 'redux-logger'
 import thunk from 'redux-thunk'
-import tomutiApp from './reducers'
+import reducers from './reducers'
 import App from './components/app'
 
 const middleware = process.env.NODE_ENV === 'production' ? [ thunk ] : [ thunk, logger() ]
 
 const store = createStore(
-  tomutiApp,
+  reducers,
   applyMiddleware(...middleware)
 )
 
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,6 @@
 import { combineReducers } from 'redux'
 import currentPomodoro from './current-pomodoro'
 
-const tomutiApp = combineReducers({
+export default combineReducers({
   currentPomodoro
 })
-
-export default tomutiApp
