3ff87cfe2529a7ca9c320aee0627a68b7dad8330
Felipe Munhoz <felipe@felipemunhoz.com>
2016-04-17 12:06:02 -0300

    Add spec to PomodoroTimer

---
+++ src/spec/components/pomodoro-timer.spec.js
@@ -0,0 +1,37 @@
+import { createElement } from 'react'
+import sd from 'skin-deep'
+
+import PomodoroTimer from '../../components/pomodoro-timer'
+import style from './../../components/pomodoro-timer/style.css'
+
+describe('PomodoroTimer', () => {
+  let component
+
+  beforeEach(() => {
+    component = sd.shallowRender(createElement(
+      PomodoroTimer, { currentMinutes: 25, currentSeconds: 0 }
+    ))
+  })
+
+  it('should show the timer with padding', () => {
+    expect(component.type).toEqual('div')
+    expect(component.props.className).toEqual(style.label)
+    expect(component.props.children).toEqual('25:00')
+  })
+})
+
+describe('PomodoroTimer', () => {
+  let component
+
+  beforeEach(() => {
+    component = sd.shallowRender(createElement(
+      PomodoroTimer, { currentMinutes: 5, currentSeconds: 20 }
+    ))
+  })
+
+  it('should show the timer with padding', () => {
+    expect(component.type).toEqual('div')
+    expect(component.props.className).toEqual(style.label)
+    expect(component.props.children).toEqual('05:20')
+  })
+})
