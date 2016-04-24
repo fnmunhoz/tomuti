4aa4b82648ee95bbda354f378139f9d029cc1586
Felipe Munhoz <felipe@felipemunhoz.com>
2016-04-24 10:16:02 -0300

    Use expression insted of string

--- src/components/pomodoro-footer/index.spec.js
+++ src/components/pomodoro-footer/index.spec.js
@@ -5,7 +5,7 @@ import PomodoroFooter from '.'
 
 describe('PomodoroFooter', () => {
   it('should show total count', () => {
-    const wrapper = shallow(<PomodoroFooter count='1' />)
+    const wrapper = shallow(<PomodoroFooter count={1} />)
 
     expect(wrapper.type()).toEqual('p')
     expect(wrapper.text()).toEqual('Total: 1')
